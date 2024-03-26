00d2ddc721ecf18ae5563b238f793b8c221e6f57
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2024-03-25 20:44:45 -0700

    used the delay in the TestAuto

--- src/main/java/frc/robot/autos/TestAuto.java
+++ src/main/java/frc/robot/autos/TestAuto.java
@@ -11,8 +11,11 @@ public class TestAuto extends SequentialCommandGroup{
     public TestAuto() {
         addCommands(
             new DriveForTime(RobotContainer.drivetrain, 1, false),
+            new Delay(0.2),
             new TurnForAngle(RobotContainer.drivetrain, 25),
+            new Delay(0.2),
             new DriveForTime(RobotContainer.drivetrain, 2, true),
+            new Delay(0.2),
             new TurnForAngle(RobotContainer.drivetrain, -30)
         );
     }
