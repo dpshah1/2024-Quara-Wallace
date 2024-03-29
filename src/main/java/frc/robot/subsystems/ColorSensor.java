396256ad14ed1e25c61d26d64a6f3b52f6155917
Dhruv Shah <dhruvpshah@gmail.com>
2024-03-29 16:39:38 -0700

    added

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -1,21 +1,35 @@
-package frc.robot;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
+package frc.robot.subsystems;
 
 import com.revrobotics.ColorSensorV3;
 
-public class ColorSensor {
-    //@Override
-    public void robotPeriodic() {
-    Color detectedColor = m_colorSensor.getColor();
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-    double IR = m_colorSensor.getIR();
+public class ColorSensor extends SubsystemBase {
+    
 
-    /*SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
-    SmartDashboard.putNumber("IR", IR);
-    */
+    ColorSensorV3 m_colorSensor;
+
+    public ColorSensor(ColorSensorV3 colorSensor)
+    {
+        m_colorSensor = colorSensor;
     }
+
+    @Override
+    public void periodic() {
+        Color detectedColor = m_colorSensor.getColor();
+
+        double IR = m_colorSensor.getIR();
+
+        SmartDashboard.putNumber("Red", detectedColor.red);
+        SmartDashboard.putNumber("Green", detectedColor.green);
+        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        SmartDashboard.putNumber("IR", IR);
+        
+    }
+
+    
+
+
 }
