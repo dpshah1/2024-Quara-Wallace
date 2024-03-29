df7cd6c53e8f33cbdc6260a44b38027503cd525d
UnseenBeans7 <112667610+UnseenBeans7@users.noreply.github.com>
2024-03-29 16:21:32 -0700

    Change intake motor types to cansparkmax

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,17 +1,19 @@
 package frc.robot.subsystems;
-
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkLowLevel;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase{
-    private final WPI_TalonSRX topIntakeMotor;
-    private final WPI_TalonSRX bottomIntakeMotor;
+    private final CANSparkMax topIntakeMotor;
+    private final CANSparkMax bottomIntakeMotor;
 
     public Intake() {
-        topIntakeMotor = new WPI_TalonSRX(Constants.TopIntakeMotor);
-        bottomIntakeMotor = new WPI_TalonSRX(Constants.BottomIntakeMotor);
+        topIntakeMotor = new CANSparkMax(Constants.TOP_INTAKE_PORT, CANSparkLowLevel.MotorType.kBrushless);
+        bottomIntakeMotor = new CANSparkMax(Constants.BOTTOM_INTAKE_PORT, CANSparkLowLevel.MotorType.kBrushless);
     }
 
     public Command runIntake() {
