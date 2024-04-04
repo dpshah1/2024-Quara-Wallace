f6df469d7d3b0f21b6dc9f33f415bcb5be95d084
Dhruv Shah <dhruvpshah@gmail.com>
2024-04-04 15:33:51 -0700

    sasdfd

---
+++ src/main/java/frc/robot/autos/CenterAutoTwoNote.java
@@ -0,0 +1,13 @@
+package frc.robot.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class CenterAutoTwoNote extends SequentialCommandGroup {
+    public CenterAutoTwoNote() {
+        addCommands(
+            
+        
+        );
+    }
+    
+}
